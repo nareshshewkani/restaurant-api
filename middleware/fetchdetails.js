ab5c861f0eb7b724984f2687a9f1c24bd6c69ed1
nareshshewkani <98037929+nareshshewkani@users.noreply.github.com>
2023-05-02 01:15:06 +0530

    Update fetchdetails.js

--- middleware/fetchdetails.js
+++ middleware/fetchdetails.js
@@ -1,6 +1,6 @@
 var jwt = require('jsonwebtoken');
 const jwtSecret = "HaHa"
-const fetch = (req,res,next)=>{
+const fetch = (req,res,next )=>{
     // get the user from the jwt token and add id to req object
     const token = req.header('auth-token');
     if(!token){
